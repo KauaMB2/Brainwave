f3a8fb4a6a57ca0632f96d5b3e873fe534081c89
KauaMB2 <i.kauamoreirabatista@gmail.com>
2024-08-14 11:22:00 -0300

    Added some sections - 06:00:00

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,6 @@
 import SectionSvg from './../assets/svg/SectionSvg'
 
-const Section = ({clasname, id, crosses, crossesOffset, customPaddings, children, className}) => {
+const Section = ({id, crosses, crossesOffset, customPaddings, children, className}) => {
   return (
     <div className={`relative ${customPaddings || `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""} ${className || ""}`}`}>
         {children}
